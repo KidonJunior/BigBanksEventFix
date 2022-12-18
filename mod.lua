f8514d2ea8a0ddb7d2c87ce7b02b743c589229f9
KidonJunior <116606981+KidonJunior@users.noreply.github.com>
2022-12-17 23:29:58 -0300

    Add files via upload

--- mod.lua
+++ mod.lua
@@ -1,8 +1,7 @@
 Hooks:PostHook(MutatorTweakData, "init_cg22", "BLEH", function(self, tweak_data)
-	self.cg22.sled_level_coordinates = {
-		big = {
+	self.cg22.sled_level_coordinates.big = {
 			position = Vector3(2250.00, -2100.00, -1007.00),
 			rotation = Rotation(0, 0, -0)
 		}
-	}
+	
 end)
